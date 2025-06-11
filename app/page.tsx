d0b1080d218d23eba9a1f9ae753ceeec9bb73e7a
VegetableDev0701 <dreamsky0701@hotmail.com>
2025-06-11 05:29:16 -0700

    3434

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import HelloWorld from "@/bookshop/components/hello.bookshop";
+import HelloWorld from "@/bookshop/components/hello/hello.bookshop";
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
